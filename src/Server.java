2e6ba3dbf4364c382e7262cf556af796e2cfff1e
kim4275 <kim4275@purdue.edu>
2023-12-10 14:08:40 -0500

    fixed some invisiible stuff

--- src/Server.java
+++ src/Server.java
@@ -169,16 +169,20 @@ public class Server extends Thread {
                 } else if (command.equals("Toggle Invisible")) {
                     String ID = reader.readLine();  // ID
                     String invisible = reader.readLine();   // name to be invisible to
-                    String invisibleID = Account.IDofUsername(invisible);   // ID to be invisible to
-                    if (invisibleID == null) {
-                        invisibleID = Account.toUserID(Account.IDofStorename(invisible));
-                    }
-                    if (Account.userCantSeeUser(invisibleID, ID)) {
-                        Account.uninvisible(ID, invisibleID);
-                        writer.println("Visible");
+                    if (!invisible.isEmpty()) {
+                        String invisibleID = Account.IDofUsername(invisible);   // ID to be invisible to
+                        if (invisibleID == null) {
+                            invisibleID = Account.toUserID(Account.IDofStorename(invisible));
+                        }
+                        if (Account.userCantSeeUser(invisibleID, ID)) {
+                            Account.uninvisible(ID, invisibleID);
+                            writer.println("Visible");
+                        } else {
+                            Account.invisible(ID, invisibleID);
+                            writer.println("Invisible");
+                        }
                     } else {
-                        Account.invisible(ID, invisibleID);
-                        writer.println("Invisible");
+                        writer.println("");
                     }
                     writer.flush();
                 } else if (command.equals("Search Users")) {
@@ -291,13 +295,13 @@ public class Server extends Thread {
         Account.resetAccountsData();
         Account.createAccount("Customer1", "customer1@gmail.com", "Customer1", "Customer"); // ID = 0
         Account.createAccount("Seller1", "seller1@gmail.com", "Seller1", "Seller");  // ID = 2
-        Account.createAccount("Seller2", "seller2@gmail.com", "Seller2", "Seller");  // ID = 4
-        Account.createAccount("Seller3", "seller3@gmail.com", "Seller3", "Seller");  // ID = 6
-        Store.createStore("2", "Store1", "Seller1's first store");      // ID = 1
-        Store.createStore("2", "Store2", "Seller1's second store");     // ID = 3
-        Store.createStore("2", "Store3", "Seller1's third store");      // ID = 5
-        Store.createStore("4", "Store4", "Seller2's store");    // ID = 7
-        Store.createStore("6", "Store5", "Seller3's store");    // ID = 9
+//        Account.createAccount("Seller2", "seller2@gmail.com", "Seller2", "Seller");  // ID = 4
+//        Account.createAccount("Seller3", "seller3@gmail.com", "Seller3", "Seller");  // ID = 6
+//        Store.createStore("2", "Store1", "Seller1's first store");      // ID = 1
+//        Store.createStore("2", "Store2", "Seller1's second store");     // ID = 3
+//        Store.createStore("2", "Store3", "Seller1's third store");      // ID = 5
+//        Store.createStore("4", "Store4", "Seller2's store");    // ID = 7
+//        Store.createStore("6", "Store5", "Seller3's store");    // ID = 9
 
         while (true) {
             try (ServerSocket serverSocket = new ServerSocket(4242)) {
